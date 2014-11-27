20c820b4b0b15bbdf1b5bbbd2ac74719307cf527
blackbbc <505968815@qq.com>
2014-11-28 00:14:02 +0800

    File

--- gen/com/example/recoder/R.java
+++ gen/com/example/recoder/R.java
@@ -1653,7 +1653,7 @@ containing a value of this type.
         public static final int action_mode_bar=0x7f09002e;
         public static final int action_mode_bar_stub=0x7f09002d;
         public static final int action_mode_close_button=0x7f090020;
-        public static final int action_settings=0x7f090043;
+        public static final int action_settings=0x7f090046;
         public static final int activity_chooser_view_content=0x7f090021;
         public static final int always=0x7f090014;
         public static final int beginning=0x7f09001b;
@@ -1675,6 +1675,7 @@ containing a value of this type.
         public static final int ifRoom=0x7f090016;
         public static final int image=0x7f090023;
         public static final int listMode=0x7f09000a;
+        public static final int listView1=0x7f090044;
         public static final int list_item=0x7f090025;
         public static final int middle=0x7f09001d;
         public static final int never=0x7f090017;
@@ -1683,6 +1684,7 @@ containing a value of this type.
         public static final int progress_circular=0x7f090007;
         public static final int progress_horizontal=0x7f090008;
         public static final int radio=0x7f09002b;
+        public static final int scrollView1=0x7f090043;
         public static final int search_badge=0x7f090035;
         public static final int search_bar=0x7f090034;
         public static final int search_button=0x7f090036;
@@ -1701,9 +1703,10 @@ containing a value of this type.
         public static final int split_action_bar=0x7f090004;
         public static final int submit_area=0x7f09003c;
         public static final int tabMode=0x7f09000c;
+        public static final int textView1=0x7f090045;
         public static final int title=0x7f090027;
-        public static final int toggleButton1=0x7f09003f;
-        public static final int toggleButton2=0x7f090040;
+        public static final int toggleButton1=0x7f090040;
+        public static final int toggleButton2=0x7f09003f;
         public static final int up=0x7f090001;
         public static final int useLogo=0x7f090013;
         public static final int withText=0x7f090018;
@@ -1759,7 +1762,8 @@ containing a value of this type.
         public static final int abc_search_view=0x7f030015;
         public static final int abc_simple_dropdown_hint=0x7f030016;
         public static final int activity_main=0x7f030017;
-        public static final int support_simple_spinner_dropdown_item=0x7f030018;
+        public static final int my_listview=0x7f030018;
+        public static final int support_simple_spinner_dropdown_item=0x7f030019;
     }
     public static final class menu {
         public static final int main=0x7f0c0000;
--- src/com/example/recoder/MainActivity.java
+++ src/com/example/recoder/MainActivity.java
@@ -1,13 +1,23 @@
 package com.example.recoder;
 
+import java.io.File;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 
 import android.media.MediaRecorder;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v7.app.ActionBarActivity;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.ToggleButton;
 
@@ -17,6 +27,10 @@ public class MainActivity extends ActionBarActivity {
 	private ToggleButton record,control;
 	private Button stop;
 	private MediaRecorder recorder;
+	private ListView list;
+	private ArrayList<String> mylist;
+	private ArrayAdapter<String> adapter;
+	private String songPath;
 	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +40,13 @@ public class MainActivity extends ActionBarActivity {
         record=(ToggleButton)findViewById(R.id.toggleButton1);
         control=(ToggleButton)findViewById(R.id.toggleButton2);
         stop=(Button)findViewById(R.id.button1);
+        list=(ListView)findViewById(R.id.listView1);
+        mylist=new ArrayList<String>();
+        
+        adapter=new ArrayAdapter<>(this, android.R.layout.simple_list_item_activated_1,mylist);
+        list.setAdapter(adapter);
+        
+        refreshFiles();
         
         record.setOnClickListener(new OnClickListener() {
 			
@@ -33,11 +54,10 @@ public class MainActivity extends ActionBarActivity {
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
 				if (record.isChecked()) {
-//					Toast.makeText(MainActivity.this, "被按了", Toast.LENGTH_SHORT).show();
 					startRecord();
 				} else {
-//					Toast.makeText(MainActivity.this, "弹起了", Toast.LENGTH_SHORT).show();
 					stopRecord();
+					refreshFiles();
 				}
 			}
 		});
@@ -63,15 +83,31 @@ public class MainActivity extends ActionBarActivity {
 				
 			}
 		});
+        
+        list.setOnItemClickListener(new OnItemClickListener() {
+
+			@Override
+			public void onItemClick(AdapterView<?> parent, View view,
+					int position, long id) {
+				// TODO Auto-generated method stub
+				songPath=(String) list.getItemAtPosition(position);
+				songPath=Environment.getExternalStorageDirectory().getPath()+"/sound_recorder/"+songPath;
+			}
+		});
+        
+        
+
     }
     
     private void startRecord() {
     	recorder=new MediaRecorder();
     	recorder.setAudioSource(MediaRecorder.AudioSource.MIC);
-    	recorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
-    	recorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
-//    	recorder.setAudioChannels(2);
-    	recorder.setOutputFile("/sdcard/sound_recorder/test.amr");
+    	recorder.setOutputFormat(MediaRecorder.OutputFormat.AAC_ADTS);
+    	recorder.setAudioEncoder(MediaRecorder.AudioEncoder.AAC_ELD);
+    	recorder.setAudioChannels(2);
+    	recorder.setOutputFile(Environment.getExternalStorageDirectory().getPath()+"/sound_recorder/"+getTime()+".acc");
+    	
+    	
     	try {
     		recorder.prepare();
     		recorder.start();
@@ -83,17 +119,36 @@ public class MainActivity extends ActionBarActivity {
     }
     
     private void stopRecord() {
-    	try {
-        	recorder.stop();
-        	recorder.reset();
-        	recorder.release();
-    	} catch (Exception e) {
-    		e.printStackTrace();
-    	}
+    	
+    	
+    	
+        recorder.stop();
+        recorder.reset();
+        recorder.release();
     }
     
     private void startPlay() {
     	
+    }
+    
+    private void refreshFiles() {
+    	String dir=Environment.getExternalStorageDirectory().getPath()+"/sound_recorder";
+        File []files=new File(dir).listFiles();
+        String result;
+        mylist.clear();
+        
+        for (File file:files) {
+        	mylist.add(file.getName());
+        }
+        
+    	adapter.notifyDataSetChanged();
+    }
+    
+    private String getTime() {
+    	SimpleDateFormat formatter=new SimpleDateFormat("yyyy年MM月dd日HH:mm:ss");
+    	Date curDate=new Date(System.currentTimeMillis());
+    	String str=formatter.format(curDate);
+    	return str;
     }
 
     
